e8b83cc721634f4dd5563ed9958da38657287a24
ddayin <ddayin@hotmail.com>
2024-06-06 13:28:40 +0900

    remove inventory system which is downloaded from asset store

--- Assets/Scripts/InventoryItem.cs
+++ Assets/Scripts/InventoryItem.cs
@@ -1,9 +1,31 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace InventoryModule
 {
     public class InventoryItem : MonoBehaviour
     {
+        private Image m_Icon;
 
+        private void Awake()
+        {
+            m_Icon = GetComponent<Image>();
+        }
+
+        private void OnMouseOver()
+        {
+            
+        }
+
+        private void OnMouseExit()
+        {
+            
+        }
+
+        public void SetIcon(Sprite _icon)
+        {
+            m_Icon.sprite = _icon;
+        }
     }
 }
